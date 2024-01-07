130741752b158da1b3914ff4f3549998625c10bd
David Finch <david.finch@databricks.com>
2024-01-07 15:11:01 -0500

    update

--- src/uc_permissions/grant_permissions.py
+++ src/uc_permissions/grant_permissions.py
@@ -7,7 +7,7 @@ from pyspark.sql import SparkSession
 
 def grant_permissions() -> None:
     """Grants a set of permissions on an object to a list of principals(users, groups, or service principals)
-    If you would like to set different permissions for different principals or set of principals, then create 
+    If you would like to set different permissions for different principals or set of principals, then create
     a separate entry for each set of permissions. Remember that permissions are hierarchical. If you grant a
     principal permissions on a Catalog, then they will have at least that level of permissions on all objects
     in the Catalog.
@@ -30,24 +30,32 @@ def grant_permissions() -> None:
         object_type = permission["object_type"]
         grants = ", ".join(str(grant) for grant in permission["grants"])
         principals = permission["principals"]
-        if revoke_permissions== True:
+        if revoke_permissions == True:
             for principal in principals:
                 try:
                     spark.sql(
-                        f"REVOKE {grants} ON `{object_name}` FROM `{principal}`"
+                        f"REVOKE {grants} ON {object_type} `{object_name}` FROM `{principal}`"
                     )
-                    print(f"Successful REVOKE {grants} ON {object_type} `{object_name}` FROM `{principal}`")
+                    print(
+                        f"Successful REVOKE {grants} ON {object_type} `{object_name}` FROM `{principal}`"
+                    )
                 except:
-                    print(f"Failed REVOKE {grants} ON {object_type} `{object_name}` FROM `{principal}`")
+                    print(
+                        f"Failed REVOKE {grants} ON {object_type} `{object_name}` FROM `{principal}`"
+                    )
         else:
             for principal in principals:
                 try:
                     spark.sql(
                         f"GRANT {grants} ON {object_type} `{object_name}` TO `{principal}`"
                     )
-                    print(f"Successful GRANT {grants} ON {object_type} `{object_name}` TO `{principal}`")
+                    print(
+                        f"Successful GRANT {grants} ON {object_type} `{object_name}` TO `{principal}`"
+                    )
                 except:
-                    print(f"Failed GRANT {grants} ON {object_type} `{object_name}` TO `{principal}`")
+                    print(
+                        f"Failed GRANT {grants} ON {object_type} `{object_name}` TO `{principal}`"
+                    )
 
 
 if __name__ == "__main__":
